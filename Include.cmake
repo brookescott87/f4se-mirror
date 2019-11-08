5971e116016b4de04420b98cad2f7896f170a8f5
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2019-11-08 13:09:36 -0500

    added build directory argument so that it can be included out-of-tree

--- Include.cmake
+++ Include.cmake
@@ -1,4 +1,4 @@
-add_subdirectory( ${CMAKE_CURRENT_LIST_DIR}/src )
+add_subdirectory( ${CMAKE_CURRENT_LIST_DIR}/src f4se-mirror )
 include_directories(
     ${CMAKE_CURRENT_LIST_DIR}/src
     ${CMAKE_CURRENT_LIST_DIR}/src/f4se
