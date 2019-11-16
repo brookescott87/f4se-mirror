f9fa0929fbb762af5d16e72f3b387a0c0c1c969c
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2019-11-16 11:06:19 -0500

    Renamed F4SE_VERSION variables to F4SE_RUNTIME_VERSION to avoid confusion with actual F4SE version.

--- src/version.cmake
+++ src/version.cmake
@@ -1,8 +1,8 @@
 set(F4SE_RUNTIME_VERSION_REGEX "^#define CURRENT_RELEASE_RUNTIME[ \t]+RUNTIME_VERSION_(([0-9]+)_([0-9]+)_([0-9]+)(_([0-9]+))?)()()[ \t]*(/.*)?$")
 
 file(STRINGS f4se/f4se_common/f4se_version.h F4SE_VERSION_LINE REGEX "${F4SE_RUNTIME_VERSION_REGEX}")
-string(REGEX REPLACE "${F4SE_RUNTIME_VERSION_REGEX}" "\\1" F4SE_VERSION_TAG "${F4SE_VERSION_LINE}")
-string(REPLACE "_" "." F4SE_VERSION_STR ${F4SE_VERSION_TAG})
+string(REGEX REPLACE "${F4SE_RUNTIME_VERSION_REGEX}" "\\1" F4SE_RUNTIME_VERSION_TAG "${F4SE_VERSION_LINE}")
+string(REPLACE "_" "." F4SE_RUNTIME_VERSION_STR ${F4SE_RUNTIME_VERSION_TAG})
 
 macro( mk_version output_var a b c d )
     math( EXPR
@@ -20,4 +20,4 @@ macro( runtime_version output_var )
     )
 endmacro()
 
-runtime_version( F4SE_VERSION_HEX )
+runtime_version( F4SE_RUNTIME_VERSION_HEX )
